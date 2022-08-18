8758680cbb4ea50978922a3d5784d5c588b241d5
= <=>
2022-08-18 14:57:34 +0300

    Added navbar

---
+++ src/Components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from '../assets/logo.png';
+import './style.css'
+
+function Header() {
+  return (
+    <div class="header">
+    <div class="inner-header">
+    <div class="logo">
+      <img src= { logo } width="70px" height="70px" />
+    </div>
+
+    <div class="nav">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">All Posts</a></li>
+        <li><a href="#">Labels</a></li>
+      </ul>
+    </div>
+    </div>
+  </div>
+  )
+}
+
+export default Header
